1383191f265a0917bb220b13838f9ae92622a84b
charleysmithschofield <charleysmithschofield@gmail.com>
2024-03-20 19:23:28 -0700

    implement two functions, one to create a tweet element and the other to render it

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,48 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+// Declare a function called renderTweets that takes tweets as its argument
+const renderTweets = function(tweets) {
+  // Loop through each tweet in the array
+  tweets.forEach(tweet => {
+    // Call createTweetElement to create a tweet element for the current tweet
+    const $tweet = createTweetElement(tweet);
+    // Append the created tweet element to the #tweets-container
+    $('#tweets-container').append($tweet);
+  });
+};
+
+// Define function called createTweetElement that takes in a tweet as its argument
+const createTweetElement = function(tweet) {
+  // Create hardcoded tweets
+  const $tweet = $(`<article class="tweet">Hello world</article>`);
+  return $tweet;
+};
+
+renderTweets(data);
